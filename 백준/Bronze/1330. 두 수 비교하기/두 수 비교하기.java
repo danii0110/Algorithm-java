a4904d4636bf55b6acec5d75c01ad80f8a5c996b
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-02-07 17:39:53 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        if (x > y) {
-            System.out.println(">");
-        } else if (x < y) {
-            System.out.println("<");
-        } else {
-            System.out.println("==");
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        System.out.println((A > B) ? ">" : ((A < B) ? "<" : "=="));
     }
 }
