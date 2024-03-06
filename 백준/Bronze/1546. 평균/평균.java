cb93614ad5a3af3bca157ad842476719e30df288
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-03-06 16:41:27 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -6,25 +6,19 @@ public class Main {
 
         //과목의 개수(N) 입력받기
         int N = Integer.parseInt(br.readLine());
-        double[] arr = new double[N];
-        double max = 0;
+
+        int max = 0;
         double total = 0;
 
         //성적 입력받기
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
-            arr[i] = Double.parseDouble(st.nextToken());
-            //점수 중 최댓값 구하기(M)
-            if(arr[i] > max) max = arr[i];
+            int val = Integer.parseInt(st.nextToken());
+
+            if(val > max) max = val;
+            total += val;
         }
 
-        //모든 점수를 (점수/M*100)으로 고치기
-        for(int i = 0; i < N; i++) {
-            arr[i] = arr[i] / max * 100;
-            total += arr[i];
-        }
-
-        //새로운 평균 계산
-        System.out.println(total / N);
+        System.out.println((total / max * 100 / N));
     }
 }
