0d1b61c88c02a6b7432971c4e21059605c0eb66d
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-03-07 14:17:03 +0900

    [Bronze V] Title: 문자와 문자열, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,21 +1,17 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //단어 S 입력받기
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String str = new String(st.nextToken());
-        char[] charArr = str.toCharArray();
+        char S [] = new char[1000];
+        S = br.readLine().toCharArray();
 
         //정수 i 입력받기
         int i = Integer.parseInt(br.readLine());
 
         //S의 i번째 글자 추출
-        System.out.println(charArr[i - 1]);
+        System.out.println(S[i - 1]);
     }
 }
