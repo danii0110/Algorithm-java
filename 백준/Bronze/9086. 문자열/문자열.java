be4d1e8537c6678acd1cf65217cbac42cc833339
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-03-08 14:07:16 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 13988 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -5,17 +5,15 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        //테스트 케이스의 개수 T 입력받기
         int T = Integer.parseInt(br.readLine());
 
-        //T만큼 반복하여 한 줄에 하나의 문자열 받기
-        //각 테케에 대해 주어진 문자열의 첫 글자와 마지막 글자 연속 출력
         for(int i = 0; i < T; i++) {
             String str = br.readLine();
-            System.out.print(str.charAt(0));
-            System.out.print(str.charAt(str.length() - 1) + "\n");
+            sb.append(str.charAt(0)).append(str.charAt(str.length() - 1)).append("\n");
         }
+        System.out.println(sb.toString());
 
     }
 }
