76f80e8db1f09d65d43a3e99004846a061edabb0
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-03-14 17:38:28 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -6,18 +6,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
-        char[] chArr = str.toCharArray();
         boolean isPalindrome = true;
 
         for(int i = 0; i < (str.length() / 2); i++) {
             if(str.charAt(i) != str.charAt(str.length() - i - 1)) {
                 isPalindrome = false;
-                break;
             }
         }
-        if (isPalindrome)
-            System.out.println("1");
-        else
-            System.out.println("0");
+        System.out.println(isPalindrome ? "1" : "0");
     }
 }
