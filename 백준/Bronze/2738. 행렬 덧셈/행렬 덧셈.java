e4708ae9e0e5d99a5689e3ff5cf31da0b2c36d57
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-03-28 10:44:43 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 216 ms, Memory: 18124 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -6,11 +6,11 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        //행렬의 크기 N, M 입력받기
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         int[][] A = new int[N][M];
-        int[][] B = new int[N][M];
 
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -21,12 +21,12 @@ public class Main {
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++) {
-                B[i][j] = Integer.parseInt(st.nextToken());
+                A[i][j] += Integer.parseInt(st.nextToken());
             }
         }
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < M; j++) {
-                bw.write(String.valueOf((A[i][j] += B[i][j])) + " ");
+                bw.write(String.valueOf(A[i][j]) + " ");
             }
             bw.write("\n");
         }
