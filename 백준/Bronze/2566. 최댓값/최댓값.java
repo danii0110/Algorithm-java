2b09936e91e6895c76a573ae90228847ce5bcac4
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-03-30 16:26:48 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 15984 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -5,16 +5,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int[][] arr = new int[9][9];
-        int max = 0;
+        int max = 0, temp;
         int maxI = 0, maxJ = 0;
 
         for(int i = 0; i < 9; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < 9; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if(arr[i][j] > max) {
-                    max = arr[i][j];
+                temp = Integer.parseInt(st.nextToken());
+                if(temp > max) {
+                    max = temp;
                     maxI = i;
                     maxJ = j;
                 }
