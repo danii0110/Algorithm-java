1c361adceb9436f38cec016dfac014e8c8927c99
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-07-24 16:46:10 +0900

    [Bronze V] Title: 문자열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.py
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -1,7 +1,4 @@
-# 테스트 케이스의 개수 T 입력받기
-# T번 반복하며 문자열 한 줄씩 입력받기
-# 각 첫 글자+마지막 글자 출력하기
 t = int(input())
-for i in range(t):
-    s = input()
-    print(s[0],s[-1], sep = '')
+for i in range (t):
+  s = input()
+  print(s[0]+s[-1])
