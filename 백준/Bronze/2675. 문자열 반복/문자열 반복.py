2abffebcacf1d65cd606dc5904ea6384769a23db
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-07-25 13:47:05 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,11 @@
+# 정수 T 입력받기(테스트 케이스의 개수)
+# T번 반복
+# 반복 횟수 R, 문자열 S 입력받기
+# 출력
+t = int(input())
+for _ in range(t):
+    p = []
+    r, s = map(str, input().split())
+    for char in s:
+      p.append(char * int(r))
+    print(*p, sep = '')
