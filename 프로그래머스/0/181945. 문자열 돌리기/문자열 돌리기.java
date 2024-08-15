64a508540b2008839eb095555c979dcc02cbc3d8
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-08-15 22:04:44 +0900

    [level 0] Title: 문자열 돌리기, Time: 150.04 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        for(char c : a.toCharArray()) {
-            System.out.println(c);
+        
+        String str = sc.next();
+        for(int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
         }
     }
 }
