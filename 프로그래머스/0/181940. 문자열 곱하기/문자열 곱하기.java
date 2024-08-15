daccfbfdf8410fe9ea2c1733beb3aa73b7d7947c
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-08-16 00:42:51 +0900

    [level 0] Title: 문자열 곱하기, Time: 1.91 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution (String my_string, int k) {
+        String ans = "";
+        
+        for (int i = 0; i < k; i++) {
+            ans += my_string;
+        }
+        
+        return ans;
+    }
+}
