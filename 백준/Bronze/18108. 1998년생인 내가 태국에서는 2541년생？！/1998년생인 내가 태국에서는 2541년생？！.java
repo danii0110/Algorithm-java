7b49f34f1a8ca45aa66544d4c454ade997bdf08a
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-02 22:38:19 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,12 +1,8 @@
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int input = sc.nextInt();
-        int res = input - 543;
-        
-        System.out.println(res);
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+        System.out.print((y - 543));
     }
 }
