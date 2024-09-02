b727299c26dc2b304cd375af5bcbf02e1b20e27a
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-02 21:35:10 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int A = sc.nextInt();
         int B = sc.nextInt();
-        int res = A + B;
-        System.out.println(res);
+        
+        System.out.println(A + B);
     }
 }
