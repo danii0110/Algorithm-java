6e28f3a6402474bdc771a70c78f7a08ce23a81b7
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-02 22:50:01 +0900

    [Bronze V] Title: 나머지, Time: 96 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,16 +1,18 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
         
-        System.out.println(((A+B)%C));
-        System.out.println((((A%C) + (B%C))%C));
-        System.out.println(((A*B)%C));
-        System.out.println((((A%C) * (B%C))%C));
+        sb.append((A+B)%C).append('\n').append(((A%C) + (B%C))%C).append('\n').append((A*B)%C).append('\n').append( ((A%C) * (B%C))%C);
+        
+        System.out.print(sb);
     }
 }
