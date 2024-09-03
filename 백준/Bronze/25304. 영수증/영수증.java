567bc461b9b56ffac7563454640c7cc6e8c4afb6
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-03 20:46:39 +0900

    [Bronze IV] Title: 영수증, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -7,11 +7,10 @@ class Main {
         int N = Integer.parseInt(br.readLine());
         int amount = 0;
         
+        StringTokenizer st;
         for(int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            amount += a * b;
+            st = new StringTokenizer(br.readLine());
+            amount += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
         }
         
         String ans = X == amount ? "Yes" : "No";
