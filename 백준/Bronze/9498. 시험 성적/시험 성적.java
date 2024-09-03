42cb8f44e6ec9ce54cf225e677f32d32fbd10734
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-03 14:35:16 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -3,8 +3,13 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int score = Integer.parseInt(br.readLine());
+        char grade;
         
-        char grade = score >= 90 ? 'A' : (score >= 80 ? 'B' : (score >= 70 ? 'C' : (score >= 60 ? 'D' : 'F')));
+        if (score >= 90) grade = 'A';
+        else if (score >= 80) grade = 'B';
+        else if (score >= 70) grade = 'C';
+        else if (score >= 60) grade = 'D';
+        else grade = 'F';
         System.out.print(grade);
     }
 }
