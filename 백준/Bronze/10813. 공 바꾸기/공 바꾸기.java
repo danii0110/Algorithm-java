165bcd0c684f887704d031fb78832eb2fe29127c
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-05 01:11:35 +0900

    [Bronze II] Title: 공 바꾸기, Time: 100 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,39 +1,33 @@
 import java.io.*;
 import java.util.StringTokenizer;
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        //N(바구니 개수), M(교환 수) 입력받음
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[N];
-
-        //각 바구니에 공 넣기(초기화)
-        for(int i = 0; i < N; i++) {
-            arr[i] = (i + 1);
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
         }
-        //M번 반복하면서 방법 받고, 공 교환
-        for(int j = 0; j < M; j++) {
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            //교환 방법 받기
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int temp;
 
-            //공을 바꾸는 데 임시저장할 공간
-            int temp = arr[a - 1];
-            arr[a - 1] = arr[b - 1];
-            arr[b - 1] = temp;
+            temp = arr[x - 1];
+            arr[x - 1] = arr[y - 1];
+            arr[y - 1] = temp;
         }
 
-        //출력
-        for(int i = 0; i < N; i++) {
-            sb.append(arr[i]).append(" ");
+        for (int a : arr) {
+            sb.append(a).append(" ");
         }
-        System.out.println(sb);
-
+        System.out.print(sb);
     }
 }
