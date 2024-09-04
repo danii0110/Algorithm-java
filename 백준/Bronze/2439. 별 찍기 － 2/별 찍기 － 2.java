4bc71ce4218b66cb137806beda06acf3dc413f4c
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-04 15:54:40 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,18 +1,15 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
-public class Main {
+import java.io.*;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < (n - i - 1); j++) {
+        StringBuffer sb = new StringBuffer();
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= (N - i); j++) {
                 sb.append(" ");
             }
-            for(int k = 0; k <= i; k++) {
+            for (int j = 1; j <= i; j++) {
                 sb.append("*");
             }
             sb.append("\n");
