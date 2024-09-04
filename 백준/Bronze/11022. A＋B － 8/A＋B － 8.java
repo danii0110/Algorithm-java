f197ccbb9a0ceb96ada9d3de64e93586f014f38d
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-04 14:57:23 +0900

    [Bronze V] Title: A+B - 8, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,17 +1,18 @@
 import java.io.*;
 import java.util.StringTokenizer;
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
         
-        for(int i = 1; i <= t; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-        
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            
-            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a + b));
+        for(int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append("Case #").append(i).append(": ").append(A).append(" + ").append(B).append(" = ").append(A + B).append("\n");
         }
+        System.out.print(sb);
     }
 }
