3a7b46e933fc84cf015dbf14a216f0069430a55d
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-05 00:34:15 +0900

    [Bronze III] Title: 최소, 최대, Time: 468 ms, Memory: 91848 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,24 +1,19 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
-public class Main {
+import java.io.*;
+import java.util.*;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        for(int i : arr) {
-            min = Math.min(min, i);
-            max = Math.max(max, i);
-        }
-        
-        System.out.print(min + " " + max);
+        sb.append(Arrays.stream(arr).min().getAsInt()).append(" ").append(Arrays.stream(arr).max().getAsInt());
+        System.out.print(sb);
     }
 }
