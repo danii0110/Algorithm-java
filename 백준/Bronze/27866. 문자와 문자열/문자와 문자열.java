3c513892f4df2487d774cf956f4ecb4e752fe258
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-05 22:55:44 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,17 +1,10 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-public class Main {
+import java.io.*;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        char S [] = new char[1000];
-        S = br.readLine().toCharArray();
-
-        //정수 i 입력받기
+        String s = br.readLine();
         int i = Integer.parseInt(br.readLine());
-
-        //S의 i번째 글자 추출
-        System.out.println(S[i - 1]);
+        
+        System.out.print(s.charAt(i - 1));
     }
 }
