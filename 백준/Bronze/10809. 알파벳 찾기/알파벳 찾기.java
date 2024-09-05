50f8712f54b404b10d58d821d96f2ac9a6a87768
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-05 23:54:44 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,28 +1,23 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-public class Main {
+import java.io.*;
+import java.util.Arrays;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        
         int[] arr = new int[26];
-
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = -1;
+        Arrays.fill(arr, -1);
+        
+        
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (arr[ch - 'a'] == -1) arr[ch - 'a'] = i;
         }
-
-        //단어 S 입력받음
-        String S = br.readLine();
-
-        //각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력
-        for(int i = 0; i < S.length(); i++) {
-            char c = S.charAt(i);
-            if(arr[c - 97] == -1) {
-                arr[c - 97] = i;
-            }
+       
+        for (int i = 0; i < 26; i++) {
+            sb.append(arr[i]).append(" ");
         }
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        System.out.print(sb);
     }
 }
