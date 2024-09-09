00878603cc7919823098e216515ffaaf9d585e3b
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-09 15:18:07 +0900

    [Bronze II] Title: 상수, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,24 +1,12 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
-public class Main {
+import java.io.*;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int x = Integer.parseInt(st.nextToken());
-        x = change(x);
-        int y = Integer.parseInt(st.nextToken());
-        y = change(y);
-
-        if(x > y) System.out.println(x);
-        else System.out.println(y);
-    }
-    private static int change(int x) {
-        int a = x / 100;
-        int b = (x % 100) / 10;
-        int c = x % 10;
-        return c * 100 + b * 10 + a;
+        String[] nums = br.readLine().split(" ");
+        
+        int num1 = Integer.parseInt(new StringBuilder(nums[0]).reverse().toString());
+        int num2 = Integer.parseInt(new StringBuilder(nums[1]).reverse().toString());
+        
+        System.out.print(Math.max(num1, num2));
     }
 }
