ac4aaf681bff239c716cba5a50731659817a9004
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-09 15:52:58 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,15 +1,8 @@
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-public class Main {
+import java.io.*;
+class Main {
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write("         ,r'\"7\n");
-        bw.write("r`-_   ,'  ,/\n");
-        bw.write(" \\. \". L_r'\n");
-        bw.write("   `~\\/\n");
-        bw.write("      |\n");
-        bw.write("      |");
-        bw.flush();
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r\'\"7\n").append("r`-_   ,\'  ,/\n").append(" \\. \". L_r\'\n").append("   `~\\/\n").append("      |\n").append("      |");
+        System.out.print(sb);
     }
 }
