5a7d0a5eed014e3653fbee09ffc9df471c643099
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-09 20:47:15 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,22 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
-public class Main {
+import java.io.*;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-
-        String[] cro = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
         
-        String[] replace = {"1", "2", "3", "4", "5", "6", "7", "8"};
+        String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
         
-        for (int i = 0; i < cro.length; i++) {
-            if (str.contains(cro[i])) {
-                str = str.replace(cro[i], replace[i]);
-            }
+        for (String c : croatia) {
+            str = str.replace(c, "*");
         }
-
-        System.out.println(str.length());
+        System.out.print(str.length());
     }
 }
