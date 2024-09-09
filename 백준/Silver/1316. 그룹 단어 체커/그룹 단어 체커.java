d16133334dc0809fe6bfbcc89e7be1584419c959
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-09 22:15:21 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,34 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-public class Main {
+import java.io.*;
+import java.util.HashSet;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int ans = 0;
 
-        int N = Integer.parseInt(br.readLine());
-        int count = 0;
-
-        for(int i = 0; i < N; i++) {
-            String str = br.readLine();
-            String[] list = str.split("");
-
-            boolean isGroupWord = true;
-
-            for(int j = 0; j < str.length() - 1; j++) {
-                if(list[j].equals(list[j + 1])) {
-                    sb.append(list[j]);
-                } else if (!(sb.toString().contains(list[j + 1]))) {
-                    sb.append(list[j]);
-                } else {
-                    isGroupWord = false;
-                    break;
-                }
+        for (int i = 0; i < n; i++) {
+            String word = br.readLine();
+            if (isGroupWord(word)) {
+                ans++;
             }
-            if(isGroupWord) count++;
-            sb.setLength(0);
         }
 
-        System.out.println(count);
+        System.out.print(ans);
+    }
+
+    public static boolean isGroupWord(String word) {
+        boolean[] visited = new boolean[26];
+        char prev = ' ';
+
+        for (int i = 0; i < word.length(); i++) {
+            char current = word.charAt(i);
+            
+            if (current != prev && visited[current - 'a']) {
+                return false;
+            }
+            visited[current - 'a'] = true;
+            prev = current;
+        }
+        return true;
     }
 }
