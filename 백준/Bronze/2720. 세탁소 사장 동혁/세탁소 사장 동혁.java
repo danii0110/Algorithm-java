25dac3d7201483197746a6ab5e55f45eb9c9c39b
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-10 20:29:06 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,20 +1,14 @@
 import java.io.*;
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int quarter = 25;
-        int dime = 10;
-        int nickel = 5;
-
         int T = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < T; i++) {
-            int C = Integer.parseInt(br.readLine());
-            sb.append(C / quarter).append(" ").append((C % quarter) / dime).append(" ").append(((C % quarter) % dime) / nickel).append(" ").append(((C % quarter) % dime) % nickel);
-            System.out.println(sb);
-            sb.setLength(0);
+        
+        for (int i = 0; i < T; i++) {
+            int c = Integer.parseInt(br.readLine());
+            sb.append(c / 25).append(" ").append(c % 25 / 10).append(" ").append(c % 25 % 10 / 5).append(" ").append(c % 5).append("\n");
         }
+        System.out.print(sb);
     }
 }
