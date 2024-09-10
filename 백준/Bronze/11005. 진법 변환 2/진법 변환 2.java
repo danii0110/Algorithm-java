77a5db713d8f69887a76573d9d7a0f4738274798
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-10 20:18:23 +0900

    [Bronze I] Title: 진법 변환 2, Time: 96 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,14 +1,24 @@
 import java.io.*;
 import java.util.StringTokenizer;
-
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        long n = Long.parseLong(st.nextToken()); // n의 범위가 크므로 long 사용
+        int n = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        System.out.print(Long.toString(n, b).toUpperCase());
+        while (n > 0) {
+            int rest = n % b;
+            if (rest < 10)
+                sb.append(rest);
+            else {
+                char c = (char) (rest + 55);
+                sb.append(c);
+            }
+            n /= b;
+        }
+        System.out.print(sb.reverse());
     }
 }
