b1b27ec151c7b4db3b54674c107977abf73f0a6e
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-10 17:58:28 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,33 +1,27 @@
 import java.io.*;
-import java.util.StringTokenizer;
-public class Main {
+import java.util.*;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] paper = new boolean[100][100];
+        int area = 0;
         
-        //색종이의 수 입력받기
-        int amount = Integer.parseInt(br.readLine());
-        //색칠한 넓이 초기화
-        int coloredArea = 0;
-        //도화지
-        boolean[][] mem = new boolean[101][101];
-        
-        //개수만큼 영역 입력받기
-        for(int i = 0; i < amount; i++) {
-            //가로 세로 입력 받기
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             
-            for(int j = x; j < x + 10; j++) {
-                for(int k = y; k < y + 10; k++) {
-                    if(!mem[j][k]) {
-                        mem[j][k] = true;
-                        coloredArea++;
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!paper[j][k]) {
+                        paper[j][k] = true;
+                        area++;
                     }
                 }
             }
         }
-        
-        System.out.println(coloredArea);
+        System.out.print(area);
     }
 }
