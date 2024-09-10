edee9e71e23dad52ef48f48b408640ae6359ecd3
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-10 13:45:33 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,26 +1,25 @@
 import java.io.*;
-import java.util.StringTokenizer;
-public class Main {
+import java.util.*;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int max = -1;
+        int n = 0, m = 0;
 
-        int max = 0, temp;
-        int maxI = 0, maxJ = 0;
-
-        for(int i = 0; i < 9; i++) {
+        for (int i = 1; i <= 9; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < 9; j++) {
-                temp = Integer.parseInt(st.nextToken());
-                if(temp > max) {
+            for (int j = 1; j <=9; j++) {
+                int temp = Integer.parseInt(st.nextToken());
+                if (temp > max) {
                     max = temp;
-                    maxI = i;
-                    maxJ = j;
+                    n = i;
+                    m = j;
                 }
             }
         }
-
-        System.out.println(max);
-        System.out.println((maxI + 1) + " " + (maxJ + 1));
+        sb.append(max).append("\n").append(n).append(" ").append(m);
+        System.out.print(sb);
     }
 }
