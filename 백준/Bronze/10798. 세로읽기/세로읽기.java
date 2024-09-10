0cea09932770bfbc422ba43fac20f00f89672097
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-10 16:01:39 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,27 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Main {
+import java.io.*;
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[][] arr = new char[5][];
-        
-        for(int i = 0; i < 5; i++) {
-            String line = br.readLine();
-            arr[i] = new char[line.length()];
-            for(int j = 0; j < line.length(); j++) {
-                arr[i][j] = line.charAt(j);
-            }
-        }
         StringBuilder sb = new StringBuilder();
-        for(int y = 0; y < 15; y++) {
-            for(int x = 0; x < 5; x++) {
-                if(y < arr[x].length) {
-                    sb.append(arr[x][y]);
+        String[] words = new String[5];
+        
+        for (int i = 0; i < 5; i++) {
+            words[i] = br.readLine();
+        }
+        for (int j = 0; j < 15; j++) {
+            for (int i = 0; i < 5; i++) {
+                if (j < words[i].length()) {
+                    sb.append(words[i].charAt(j));
                 }
             }
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
