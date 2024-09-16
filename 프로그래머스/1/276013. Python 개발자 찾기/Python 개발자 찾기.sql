0cf9e9f099eedda8af92e3ca075ff9318ed81b28
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-17 00:35:54 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,8 +1,9 @@
-# DEVELOPER_INFOS 테이블에서
-# 'Python' 스킬을 가진 개발자의
-# 개발자 ID, 이메일, 이름, 성 조회
-# ID를 기준으로 오름차순 정렬
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
 FROM DEVELOPER_INFOS
-WHERE 'Python' IN (SKILL_1, SKILL_2, SKILL_3)
-ORDER BY ID;
+WHERE SKILL_1 = 'Python' || SKILL_2 = 'Python' || SKILL_3 = 'Python'
+ORDER BY ID ASC;
+
+# DEVELOPER_INFOS에서
+# 'Python' 스킬을 가진 개발자의
+# ID, 이메일, 이름, 성 조회
+# ID ASC
