78de1ad94c9b6a5ce5abf0d8cc61f1aa500fd6a8
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-16 23:22:58 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,10 +1,11 @@
-# USED_GOODS_BOARD, USED_GOODS_REPLY 테이블에서
-# '2022-10'에 작성된
-# 게시글 제목, 게시글ID, 댓글ID, 댓글 작성자ID, 댓글 내용, 댓글 작성일 조회
-# 댓글 작성일으르 기준으로 오름차순 정렬
-# if) 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬
-# 날짜 형식 주의
 SELECT ugb.TITLE, ugb.BOARD_ID, ugr.REPLY_ID, ugr.WRITER_ID, ugr.CONTENTS, DATE_FORMAT(ugr.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
-FROM USED_GOODS_BOARD ugb, USED_GOODS_REPLY ugr
-WHERE DATE_FORMAT(ugb.CREATED_DATE, '%Y-%m') = '2022-10' and ugb.BOARD_ID = ugr.BOARD_ID
-ORDER BY ugr.CREATED_DATE, ugb.TITLE;
+FROM USED_GOODS_BOARD ugb
+JOIN USED_GOODS_REPLY ugr
+ON ugb.BOARD_ID = ugr.BOARD_ID
+WHERE DATE_FORMAT(ugb.CREATED_DATE, '%Y-%m') = '2022-10'
+ORDER BY CREATED_DATE ASC, ugb.TITLE ASC;
+
+# USED_GOODS_BOARD, USED_GOODS_REPLY에서
+# 2022년 10월에 작성된
+# 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일 조회
+# 댓글 작성일 ASC, 게시글 제목 ASC
