381d10f33252b167deaf46fb91ec36facc433cd0
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-16 23:54:24 +0900

    [level 1] Title: 역순 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59035. 역순 정렬하기/역순 정렬하기.sql
+++ 프로그래머스/1/59035. 역순 정렬하기/역순 정렬하기.sql
@@ -1,6 +1,7 @@
-# 동물 보호소에 들어온
-# 모든 동물의 이름과 보호 시작일을 조회
-# 결과는 ANIMAL_ID 역순으로 정렬
 SELECT NAME, DATETIME
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID DESC;
+
+# ANIMAL_INS에서
+# 동물의 이름, 보호 시작일 조회
+# ANIMAL_ID DESC
