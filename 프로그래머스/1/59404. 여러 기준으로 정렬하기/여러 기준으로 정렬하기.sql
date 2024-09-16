91102de4f200b88e7fb29733ce03ccebec21a5e8
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-17 00:10:30 +0900

    [level 1] Title: 여러 기준으로 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
+++ 프로그래머스/1/59404. 여러 기준으로 정렬하기/여러 기준으로 정렬하기.sql
@@ -1,7 +1,7 @@
-# ANIMAL_INS 테이블을 이용하여
-# 모든 동물의 아이디, 이름, 보호 시작일을
-# 이름 순으로 조회
-# if) 이름이 같다면 보호 시작일 내림차순으로
 SELECT ANIMAL_ID, NAME, DATETIME
 FROM ANIMAL_INS
-ORDER BY NAME, DATETIME DESC;
+ORDER BY NAME ASC, DATETIME DESC;
+
+# ANIMAL_INS에서
+# 아이디, 이름, 보호 시작일 조회
+# 이름 ASC, 보호 시작일 DESC
