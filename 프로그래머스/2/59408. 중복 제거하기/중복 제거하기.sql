ca1bf8d6c56307aa97577bfdf39a18a6c8a14ae7
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-09-17 17:42:23 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,7 +1,7 @@
-# ANIMAL_INS 테이블에서
-# 동물의 이름이 몇 개인지 조회
-# 이름이 NULL인 경우는 집계 x
-# 중복 제거
 SELECT COUNT(DISTINCT NAME)
 FROM ANIMAL_INS
 WHERE NAME IS NOT NULL;
+
+# ANIMAL_INS에서
+# 동물의 이름은 몇 개인지 조회
+# if) 이름 == NULL -> 집계x
