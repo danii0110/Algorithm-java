e1bd5d2963f40cf552e6ec281a3b083b39020218
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-11-12 10:29:03 +0900

    [Bronze III] Title: 세 막대, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.java
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -3,22 +3,29 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
+        int s = 0;
         
-        System.out.print(maxTriangle(a, b, c));
+        if ( a == b && b == c && c == a ) s = a + b + c;
+        else if ( a + b <= c ) {
+            c = a + b - 1;
+            s = a + b + c;
+        }
+        else if ( a + c <= b ) {
+            b = a + c - 1;
+            s = a + b + c;
+        }
+        else if ( b + c <= a ) {
+            a = b + c - 1;
+            s = a + b + c;
+        }
+        else s = a + b + c;
+
+        System.out.print(s);
     }
-    public static int maxTriangle(int a, int b, int c) {
-    int[] sides = {a, b, c};
-    Arrays.sort(sides);
-    
-    if (sides[0] + sides[1] > sides[2]) return sides[0] + sides[1] + sides[2];
-    else {
-        sides[2] = sides[0] + sides[1] - 1;
-        return sides[0] + sides[1] + sides[2];
-    }
-}
 }
