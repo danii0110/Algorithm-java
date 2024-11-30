c2662a0ff834a3ebde5c62e29008b2aba05cefea
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-11-30 12:53:37 +0900

    [Bronze II] Title: 소수, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,38 +1,40 @@
 import java.io.*;
-import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int sum = 0, min = -1;
         
         int m = Integer.parseInt(br.readLine());
         int n = Integer.parseInt(br.readLine());
         
+        boolean[] isNotPrime = new boolean[n + 1];
+        isNotPrime[0] = isNotPrime[1] = true;
+        
+        for (int i = 2; i * i <= n; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+        
+        int sum = 0;
+        int min = -1;
+        
         for (int i = m; i <= n; i++) {
-            if (isPrime(i)) {
+            if (!isNotPrime[i]) {
                 sum += i;
-                if (min > i || min == -1) {
+                if (min == -1) {
                     min = i;
                 }
             }
         }
         
         if (sum == 0) {
-            sb.append(min);
+            sb.append(-1);
         } else {
             sb.append(sum).append("\n").append(min);
         }
         System.out.print(sb);
-    }
-    static boolean isPrime(int n) {
-        if (n <= 1) return false;
-        if (n == 2) return true;
-        for (int i = 2; i *i <= n; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
