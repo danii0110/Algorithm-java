2e8933981212568d9e039ca33f1ed00db0d36a55
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-12-07 21:57:45 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14512 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            int gcd = gcd(x, y);
+            sb.append((x * y) / gcd).append("\n");
+        }
+        System.out.print(sb);
+    }
+    static int gcd(int x, int y) {
+        while (y != 0) {
+            int temp = x % y;
+            x = y;
+            y = temp;
+        }
+        return x;
+    }
+}
