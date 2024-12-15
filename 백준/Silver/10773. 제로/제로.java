b3452467df30255764946efdaa3ad8b81bbf7f28
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-12-15 14:18:17 +0900

    [Silver IV] Title: 제로, Time: 200 ms, Memory: 23848 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -4,19 +4,20 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Stack<Integer> stack = new Stack<>();
-        int res = 0;
         int k = Integer.parseInt(br.readLine());
         
         for (int i = 0; i < k; i++) {
             int temp = Integer.parseInt(br.readLine());
             if (temp == 0) {
-                res -= stack.peek();
                 stack.pop();
             }
             else {
                 stack.push(temp);
-                res += temp;
             }
+        }
+        int res = 0;
+        for (int s : stack) {
+            res += s;
         }
         System.out.print(res);
     }
