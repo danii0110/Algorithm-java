67663e6f8960b54d7649e11fba89265cfdbddd0b
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-12-21 11:17:31 +0900

    [Silver IV] Title: 카드2, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,19 +1,12 @@
 import java.io.*;
-import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Deque<Integer> deque = new ArrayDeque<>();
-        
         int n = Integer.parseInt(br.readLine());
         
-        for (int i = 1; i <= n; i++) {
-            deque.add(i);
-        }
-        while(deque.size() > 1) {
-            deque.pollFirst();
-            deque.offerLast(deque.pollFirst());
-        }
-        System.out.print(deque.peekFirst());
+        int highestPowerOfTwo = Integer.highestOneBit(n);
+        int res = (n == highestPowerOfTwo) ? n : (n - highestPowerOfTwo) * 2;
+        
+        System.out.print(res);
     }
 }
