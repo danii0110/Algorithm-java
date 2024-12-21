3eb88c08c4522ea62a01ac44ef86b6744ceadeb8
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-12-21 19:15:05 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 152 ms, Memory: 14960 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -5,34 +5,29 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        List<Integer> values = new ArrayList<>();
-        Deque<Integer> deque = new ArrayDeque<>();
         
-        // 풍선 번호와 값을 초기화
+        int n = Integer.parseInt(br.readLine());
+        Deque<int[]> deque = new ArrayDeque<>();
+        
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            deque.add(i); // 풍선 번호 저장
-            values.add(Integer.parseInt(st.nextToken())); // 풍선 값 저장
+            int value = Integer.parseInt(st.nextToken());
+            deque.add(new int[]{i, value});
         }
         
         while (!deque.isEmpty()) {
-            // 현재 풍선 번호
-            int current = deque.pollFirst();
-            sb.append(current).append(" ");
+            int[] current = deque.pollFirst();
+            sb.append(current[0]).append(" ");
             
             if (deque.isEmpty()) break;
             
-            // 이동 값(현재 풍선의 값)
-            int move = values.get(current - 1);
+            int move = current[1];
             
             if (move > 0) {
-                // 오른쪽으로 이동
                 for (int i = 1; i < move; i++) {
                     deque.addLast(deque.pollFirst());
                 }
             } else {
-                // 왼쪽으로 이동
                 for (int i = 0; i < Math.abs(move); i++) {
                     deque.addFirst(deque.pollLast());
                 }
