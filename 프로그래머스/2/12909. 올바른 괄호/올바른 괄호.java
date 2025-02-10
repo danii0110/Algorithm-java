10e264d4e14e1e02c751382e3ec4b6f57f4ad67d
danii0110 <105475360+danii0110@users.noreply.github.com>
2025-02-10 11:43:06 +0900

    [level 2] Title: 올바른 괄호, Time: 5.78 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') count++;
+            else {
+                count--;
+                if (count < 0) return false;
+            }
+        }
+        
+        return count == 0;
+    }
+}
