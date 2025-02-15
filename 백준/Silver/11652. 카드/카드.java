9898e7dd9a88f6b766d4f2fd7f275c7204c5e9f5
danii0110 <105475360+danii0110@users.noreply.github.com>
2025-02-16 00:17:44 +0900

    [Silver IV] Title: 카드, Time: 320 ms, Memory: 31924 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,30 +1,28 @@
 import java.io.*;
 import java.util.*;
-
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<Long, Integer> map = new HashMap<>();
+        long mostFrequentNum = 0;
+        int maxCount = 0;
+        
         int n = Integer.parseInt(br.readLine());
         
-        Map<Long, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            long num = Long.parseLong(br.readLine());
+            Long num = Long.parseLong(br.readLine());
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        
-        long mostFrequentNumber = 0;
-        int maxCount = 0;
         
         for (Map.Entry<Long, Integer> entry : map.entrySet()) {
             long key = entry.getKey();
             int value = entry.getValue();
             
-            if (value > maxCount || (value == maxCount && key < mostFrequentNumber)) {
-                mostFrequentNumber = key;
+            if (value > maxCount || (value == maxCount && key < mostFrequentNum)) {
+                mostFrequentNum = key;
                 maxCount = value;
             }
         }
-        
-        System.out.println(mostFrequentNumber);
+        System.out.println(mostFrequentNum);
     }
 }
